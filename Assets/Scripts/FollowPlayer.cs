327e9518213a6744e52b7a02f402b9f0726c2118
armeentalwandi <68311871+armeentalwandi@users.noreply.github.com>
2020-07-25 10:28:22 -0400

    Moved the Prototype to Unity

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//armeen
+public class FollowPlayer : MonoBehaviour
+{
+    //creates a player object which is connected to the camera. In unity. dragged the vehicle to the player object which connects them
+    public GameObject player;
+    private Vector3 offset = new Vector3(0, 8, -15);
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //assign cameras position to the vehicles position
+        //offset the camera behind the player by adding to the players position
+        transform.position = player.transform.position + offset;
+
+
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float speed = 15.5f;
+    private float turnSpeed = 40.0f;
+
+    float horizontalInput;
+    float forwardInput;
+
+    #region Added for Hard Challenge
+    public Camera mainCamera;
+    public Camera hoodCamera;
+    public KeyCode switchKey;
+    #endregion
+
+
+    #region Added for Expert Challenge
+    public string inputID;
+    #endregion
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        #region Updated for Expert Challenge
+        // Axis setup
+        horizontalInput = Input.GetAxis("Horizontal" + inputID);
+        forwardInput = Input.GetAxis("Vertical" + inputID);
+        #endregion
+
+        // Move the vehicle forward
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+
+        // Rotate the vehicle left and right
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
+
+        #region Added for Hard Challenge
+        if (Input.GetKeyDown(switchKey))
+        {
+            mainCamera.enabled = !mainCamera.enabled;
+            hoodCamera.enabled = !hoodCamera.enabled;
+        }
+        #endregion
+    }
+}
